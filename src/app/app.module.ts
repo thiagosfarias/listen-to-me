f7b94d2458cedf3e36a8f89253ff4e88e733a22c
Thiago Farias <31117547+thiagosfarias@users.noreply.github.com>
2019-01-12 20:13:51 -0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { LibraryPage } from '../pages/library/library';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AudioProvider } from '../providers/audio/audio';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    LibraryPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -26,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    LibraryPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -34,7 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AudioProvider
   ]
 })
 export class AppModule {}
